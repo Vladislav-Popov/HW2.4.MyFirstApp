6d949cb778b23b0563db34903934ea8a51ea3196
Vladislav <vladislav.y.popov@gmail.com>
2020-10-24 21:29:02 +0300

    Log In screen

--- HW2.4.MyFirstApp/ViewController.swift
+++ HW2.4.MyFirstApp/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super .touchesBegan(touches, with: event) }
+    
+    
+    @IBAction func forgotLoginButton() {
+        showLogin(with: "You Login", and: "NNNN")
+    }
+    @IBAction func showPasswordButton() {
+    showLogin(with: "You Password", and: "222")
+    }
 }
 
+
+
+//MARK: -
+extension ViewController {
+    private func showLogin(with title: String, and message: String) {
+        let login = UIAlertController(title: title, message: message, preferredStyle: .alert)
+       
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        login.addAction(okAction)
+        present(login, animated: true)
+    }
+}
